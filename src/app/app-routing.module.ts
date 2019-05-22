490df44eba8b26aa04a37f9c3ae72bbe6b572bcf
mohd kashif <mohd.kashif.ext@siemens.com>
2019-05-22 15:15:49 +0530

    Added lazy loading for POC purpose

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BillCalculationModule } from './bill-calculation/bill-calculation.module';
 
 const routes: Routes = [{
   path: '',
-  loadChildren: 'app/bill-calculation/bill-calculation.module#BillCalculationModule'
+  loadChildren: () => BillCalculationModule
 }];
 
 @NgModule({
